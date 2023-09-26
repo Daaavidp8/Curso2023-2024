23a5628df47498fa04461f9e246cc8140e5cbc4b
David Picazo Rocafull <davpicroc@alu.edu.gva.es>
2023-09-26 20:14:30 +0200

    Loteria

--- Arrays/Loteria/euromillon.php
+++ Arrays/Loteria/euromillon.php
@@ -1,5 +1,17 @@
 <?php include "./cabecera.inc"?>
-<?php
+<?php include "./loteria.inc" ?>
 
+<div>
+<?php
+echo "<h1>Euromillon: </h1>";
 ?>
+
+    <p>
+         <?php
+             loteria(true);
+             echo '<b>-</b>';
+             loteria(false);
+        ?>
+    </p>
+</div>
 <?php include "../../pie.inc"?>
--- Arrays/Loteria/primitiva.php
+++ Arrays/Loteria/primitiva.php
@@ -1,28 +1,15 @@
 <?php include "./cabecera.inc"?>
+<?php include "./loteria.inc"?>
+
+<div>
 <?php
-
-$numeros = [];
-$cogidos = [];
-
-    function aleatorio(){
-        return rand(1,49);
-    }
-
-
-    for ($i = 0;$i <= 6;$i++){
-        $random = aleatorio();
-        do{
-            $random = aleatorio();
-        }while(!(in_array($random,$cogidos)));
-        $cogidos[] = $random;
-        $numeros[] = $random;
-    }
-
-    echo "Primitiva: ";
-    foreach ($numeros as $numero){
-        echo $numero . " ";
-    }
-
-
+echo "<h1>Primitiva: </h1>";
 ?>
+
+    <p>
+         <?php
+             loteria(true);
+        ?>
+    </p>
+</div>
 <?php include "../../pie.inc"?>
